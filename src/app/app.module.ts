52f6445eeb6a61d9b7ba3e64d6364562e65cec87
fernandocto.scz <fernandocto.scz@gmail.com>
2019-09-26 11:51:49 -0400

    :white_check_mark: Add date component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
+/** Third party modules */
+import { NgxBootstrapModule } from './ngx-bootstrap.module';
+
 import { AppComponent } from './app.component';
+import { DateComponent } from './components/date/date.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NgxBootstrapModule
   ],
   providers: [],
   bootstrap: [AppComponent]
