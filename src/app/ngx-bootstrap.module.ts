e4d5f62222659768592a09e6cd1fb7fbedb50bf4
fernandocto.scz <fernandocto.scz@gmail.com>
2019-09-26 11:56:43 -0400

    Add ngx module

---
+++ src/app/ngx-bootstrap.module.ts
@@ -0,0 +1,15 @@
+/** Modules */
+import { NgModule } from '@angular/core';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+
+
+const MODULES = [
+  BsDatepickerModule
+]
+
+@NgModule({
+  declarations: [],
+  imports: [MODULES],
+  exports: [MODULES]
+})
+export class NgxBootstrapModule { }
